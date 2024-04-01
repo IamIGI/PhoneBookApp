5ffbef7eef6a0a4a623a57f7fd3773f106770294
i.kłusek <J6ZkqwVUn>
2024-04-01 22:52:28 +0200

    PBA-01-Final app

---
+++ PhoneBookApp/Contact.cs
@@ -0,0 +1,29 @@
+﻿namespace PhoneBookApp
+{
+    internal class Contact(string name, string number)
+    {
+        public string Name { get; set; } = name;
+        public string Number { get; set; } = number;
+
+        public string this[SearchContactBy key]
+        {
+            get
+            {
+                switch (key)
+                {
+                    case SearchContactBy.Name: return Name;
+                    case SearchContactBy.Number: return Number;
+                    default:
+                        throw new ArgumentOutOfRangeException(nameof(key), key, null);
+                }
+            }
+        }
+    }
+
+    public enum SearchContactBy
+    {
+        Name,
+        Number
+    }
+
+}
